a97bc662dc715a514e55ec111263baecc7985753
rusikstayhome <alenrus32@gmail.com>
2022-08-23 16:02:13 +0300

    made an api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import { useState } from 'react';
+import axios from 'axios';
 import './Dictionary.css';
 
 const Dictionary = () => {
     let [keyword, setKeyword] = useState('');
 
+    const handleResponse = (res) => {
+        console.log(res);
+    }
+
     const search = (e) => {
         e.preventDefault();
-        alert(`searching for ${keyword} definition`)
+
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
+        axios.get(apiUrl).then(handleResponse)
     }
 
     const handleKeywordChange = (e) => {
